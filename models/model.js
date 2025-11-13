42cd7ddc70751e541e21353bdfae76db20a54ac7
diansyah06 <dianofficial06@gmail.com>
2025-11-13 23:48:11 +0700

    perbaika 01

--- models/model.js
+++ models/model.js
@@ -1,5 +1,12 @@
 import { Sequelize, DataTypes } from "sequelize";
 
+console.log("--- DEBUGGING ENV VARS ---");
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_NAME:", process.env.DB_NAME);
+console.log("DB_PORT:", process.env.DB_PORT);
+console.log("DB_PASS:", process.env.DB_PASS ? "DITERIMA" : "KOSONG/UNDEFINED");
+console.log("--- END DEBUG ---");
 
 const sequelize = new Sequelize(
   process.env.DB_NAME || "mini_store",
